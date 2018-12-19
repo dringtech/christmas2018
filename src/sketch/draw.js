557538b57579d3a21401bd9f2401a8cceaa32964
Giles Dring <gilesdring@gmail.com>
2018-12-19 13:49:06 +0000

    Sorting the skaters so they're in the right order

--- src/sketch/draw.js
+++ src/sketch/draw.js
@@ -3,6 +3,10 @@ import { skaters } from './setup'
 import { bg } from '../elements/background'
 
 export function draw () {
+  Object.keys(skaters)
+    .forEach(_ => skaters[_].animate())
   image(bg, 0, 0)
-  Object.keys(skaters).forEach(function (_) { skaters[_].animate(); skaters[_].draw() })
+  Object.keys(skaters)
+    .sort((a, b) => skaters[a].y > skaters[b].y)
+    .forEach(_ => skaters[_].draw())
 }
