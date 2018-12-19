daf19d5e1cbb07cd4cde12bb005fee4648a99695
Giles Dring <gilesdring@gmail.com>
2018-12-19 00:59:47 +0000

    Add babel

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,21 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              [ '@babel/preset-env', {
+                targets: '> 0.25%, not dead',
+                useBuiltIns: 'entry',
+              } ],
+            ],
+          },
+        },
+      },
+      {
         test: /\.css$/,
         use: [
           {
